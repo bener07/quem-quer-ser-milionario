612c8c597c14aa1a84c7ba68e27b0a8c58297a7a
pedroamado07 <pedro.correia.amado@gmail.com>
2022-11-10 10:50:46 +0000

    Acabei

--- primeira_pergunta.h
+++ primeira_pergunta.h
@@ -1,5 +1,26 @@
 #include <stdio.h>
-int main ()
+int "primeira_pergunta" ()
 {
-    printf("");
+    char r;
+    printf("Qual desta versões fez um update aquático?\n");
+    printf("Versão 1.9\n");
+    printf("Versão 1.10\n");
+    printf("Versão 1.12\n");
+    printf("Versão 1.13\n");
+    scanf("%c", &r);
+    switch (r)
+    {
+    case 'A':printf(Errado);
+        break;
+    case 'B':printf(Errado);
+        break;
+    case 'C':printf(Errado);
+        break;
+    case 'D':printf(Certo);
+        break;
+    default:printf(NÃO HÁ ESSA OPÇÃO);
+        break;
+    }
+    return 0;
+    
 }
