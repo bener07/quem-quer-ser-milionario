0eecd5bc7f1484a7542b039e619e4d84ff5c961f
pedroamado07 <pedro.correia.amado@gmail.com>
2022-11-14 13:41:53 +0000

    madeit

--- primeira_pergunta.h
+++ primeira_pergunta.h
@@ -1,26 +1,50 @@
 #include <stdio.h>
-int "primeira_pergunta" ()
-{
-    char r;
-    printf("Qual desta versões fez um update aquático?\n");
-    printf("Versão 1.9\n");
-    printf("Versão 1.10\n");
-    printf("Versão 1.12\n");
-    printf("Versão 1.13\n");
-    scanf("%c", &r);
-    switch (r)
-    {
-    case 'A':printf(Errado);
-        break;
-    case 'B':printf(Errado);
-        break;
-    case 'C':printf(Errado);
-        break;
-    case 'D':printf(Certo);
-        break;
-    default:printf(NÃO HÁ ESSA OPÇÃO);
-        break;
+
+int decima_pergunta(int pontos){
+    int confirmacao;
+    do{
+        char r;
+        puts("Qual desta versões fez um update aquático?");
+        puts("A - 1.9");
+        puts("B - 1.10");
+        puts("C - 1.12");
+        puts("D - 1.13");
+        printf("Resposta: ");
+        scanf(" %c", &r);
+        switch(r){
+            case 'A':
+            case 'a':
+                puts("Resposta Errada");
+                confirmacao = 0;
+                pontos++;
+                break;
+            case 'B':
+            case 'b':
+                puts("Resposta Errada");
+                confirmacao = 0;
+                pontos--;
+                break;
+            case 'c':
+            case 'C':
+                puts("Resposta Errada");
+                confirmacao = 0;
+                pontos--;
+                break;
+            case 'D':
+            case 'd':
+                puts("Resposta Certo");
+                confirmacao = 0;
+                pontos--;
+                break;
+            default:
+                printf("Caracter '%c' inválido\n", r);
+                confirmacao = 1;
+                break;
+        }
     }
-    return 0;
-    
+    while(confirmacao);
+    printf("Clica no ENTER para prosseguir...");
+    getchar(); // para apanhar com o ultimo \n introduzido, isto seria semelhante a scanf(" %c", input)
+    while( getchar() != '\n' );
+    return pontos;
 }
